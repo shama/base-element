84622d2ca0cc78510aaa382ab48e17f68512a890
Kyle Robinson Young <kyle@dontkry.com>
2015-05-03 12:49:47 -0700

    Add web component example

---
+++ examples/webcomponent.js
@@ -0,0 +1,28 @@
+var List = require('./list.js')
+
+var data = ['one', 'two', 'three']
+
+// Create a proto for our custom element
+var proto = Object.create(HTMLElement.prototype)
+proto.createdCallback = function () {
+  // Create a new list and append to this custom element
+  var list = new List(this)
+  list.render(data)
+
+  // When item clicked
+  list.on('clicked', function (item) {
+    window.alert('You clicked ' + item.innerHTML)
+  })
+
+  // When item added
+  list.on('added', function () {
+    data.push(String(Math.random() * 9999))
+    list.render(data)
+  })
+}
+
+// Create our tag using the above proto
+var XList = document.registerElement('x-list', {
+  prototype: proto
+})
+document.body.appendChild(new XList())
