98aa075fb8095f99bb1df5273fa2c6ce45e065ad
Kyle Robinson Young <kyle@dontkry.com>
2015-05-03 12:59:37 -0700

    Add example for react

---
+++ examples/react.js
@@ -0,0 +1,29 @@
+var List = require('./list.js')
+var React = require('react')
+
+var XList = React.createClass({
+  componentDidMount: function () {
+    var list = new List(React.findDOMNode(this))
+    var data = this.props.items
+    list.render(data)
+
+    // When item clicked
+    list.on('clicked', function (item) {
+      window.alert('You clicked ' + item.innerHTML)
+    })
+
+    // When item added
+    list.on('added', function () {
+      data.push(String(Math.random() * 9999))
+      list.render(data)
+    })
+  },
+  render: function () {
+    return React.createElement('div')
+  }
+})
+
+React.render(
+  React.createElement(XList, { items: ['one', 'two', 'three'] }),
+  document.body
+)
