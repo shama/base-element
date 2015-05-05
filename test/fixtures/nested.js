979f6f3cd64c5a2d3de8e617ece0077d0ba31296
Kyle Robinson Young <kyle@dontkry.com>
2015-05-04 19:03:59 -0700

    clean up

--- test/fixtures/nested.js
+++ test/fixtures/nested.js
@@ -6,45 +6,30 @@ function Bottom (el) {
   BaseElement.call(this, el)
 }
 Bottom.prototype = Object.create(BaseElement.prototype)
-Bottom.prototype.test = 'bottom'
 
 Bottom.prototype.render = function (data) {
-  //console.log('render bottom')
-  //return this.super(data)
   var vtree = this.html('li.bottom', data)
-  //return BaseElement.prototype.render(vtree)
-  //return this.super(vtree)
   return this.super(vtree)
-  //return vtree
 }
 
 function Middle (el) {
   Bottom.call(this, el)
 }
 Middle.prototype = Object.create(Bottom.prototype)
-Middle.prototype.test = 'middle'
 
 Middle.prototype.render = function (data) {
-  //console.log('render middle', this)
   var bottom = Bottom.prototype.render(data)
-  return this.super(this.html('ul.middle', bottom))
-  //return this.super(data)
+  var vtree = this.html('ul.middle', bottom)
+  return this.super(vtree)
 }
 
 function Top (el) {
   Middle.call(this, el)
 }
 Top.prototype = Object.create(Middle.prototype)
-Top.prototype.test = 'top'
 
 Top.prototype.render = function (data) {
   var middle = Middle.prototype.render(data)
   var vtree = this.html('div.top', middle)
-  //console.log(vtree)
   return this.super(vtree)
-
-  //return this.super(data)
-  // return this.super(this.html('.top'), [
-  //   this.super(data)
-  // ])
 }
