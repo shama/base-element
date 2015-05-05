ed97dd84267051240f9ffc8fae197dea9ade7fd3
Kyle Robinson Young <kyle@dontkry.com>
2015-05-04 19:26:03 -0700

    Add nesting example

---
+++ examples/nesting.js
@@ -0,0 +1,47 @@
+var BaseElement = require('../index.js')
+
+// First Bear extends BaseElement
+function Bear (el) {
+  BaseElement.call(this, el)
+}
+Bear.prototype = Object.create(BaseElement.prototype)
+Bear.prototype.render = function (items) {
+  var self = this
+  var vtree = items.map(function (item) {
+    return self.html('li.bear', item)
+  })
+  return this.super(vtree)
+}
+
+// Second Grizzly extends Bear
+function Grizzly (el) {
+  Bear.call(this, el)
+}
+Grizzly.prototype = Object.create(Bear.prototype)
+Grizzly.prototype.render = function (data) {
+  var bears = Bear.prototype.render(data.bears)
+  var vtree = this.html('ul.grizzly', [
+    this.html('li', 'Top'),
+    bears,
+    this.html('li', 'Bottom'),
+  ])
+  return this.super(vtree)
+}
+
+// Third App extends Grizzly
+function App (el) {
+  Grizzly.call(this, el)
+}
+App.prototype = Object.create(Grizzly.prototype)
+App.prototype.render = function (data) {
+  var middle = Grizzly.prototype.render(data)
+  var vtree = this.html('div.app', [data.heading, middle])
+  return this.super(vtree)
+}
+
+// The end user only cares about App
+var app = new App(document.body)
+app.render({
+  heading: 'Bears',
+  bears: ['Grizzly', 'Polar', 'Brown']
+})
