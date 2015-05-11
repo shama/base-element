b687a565c9d8532b4225b978610e736449cc39ba
Kyle Robinson Young <kyle@dontkry.com>
2015-05-10 20:35:16 -0700

    Update custom-element example with modifying data via attribute trigger

--- examples/custom-element.js
+++ examples/custom-element.js
@@ -5,19 +5,37 @@ require('webcomponents.js')
 var List = require('./list.js')
 var createCustom = require('custom-element')
 
+// Mock data for the element
 var data = ['one', 'two', 'three']
 
 var CustomHTMLElement = createCustom().on('created', function () {
-  var list = new List(this)
-  list.render(data)
-  list.on('clicked', function (item) {
+  var self = this
+  // When the element is created, consume List's API
+  // Attaching to the element so it can be called via the element itself
+  self.list = new List(self)
+  self.list.render(data)
+  self.list.on('clicked', function (item) {
     window.alert('You clicked ' + item.innerHTML)
   })
-  list.on('added', function () {
+  self.list.on('added', function () {
     data.push(String(Math.random() * 9999))
-    list.render(data)
+    self.list.render(data)
   })
+}).on('attribute', function (key) {
+  // When an attribute is set, add the attribute value to our data
+  var test = this.getAttribute(key)
+  data.push(test)
+  this.list.render(data)
 })
 
+// Register our custom element
 var CustomElement = document.registerElement('custom-element', CustomHTMLElement)
-document.body.appendChild(new CustomElement())
+
+// Create and append it to document.body
+var el = new CustomElement()
+document.body.appendChild(el)
+
+// 2s later, modify the attribute of the element
+setTimeout(function () {
+  el.setAttribute('data-test', 'I was added because of an attribute')
+}, 2000)
